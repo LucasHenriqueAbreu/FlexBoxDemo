cdde1c8f846346b2957f1053261049dfd3387317
Developer <lucas.abreu@softfocus.com.br>
2019-01-18 09:30:57 -0200

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FlexBoxDemo';
+  
+  itemList = [
+    {
+      title: 'Item 1',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 2',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 3',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 4',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 5',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 6',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 7',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 8',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 9',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 9',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 9',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 9',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 9',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 9',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 9',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 9',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    },
+    {
+      title: 'Item 9',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
